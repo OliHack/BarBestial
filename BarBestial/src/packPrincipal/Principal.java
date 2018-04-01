4440cb9afd5159e69b1e4aad93f80f39025f10ef
Oliver Ye <olizye@gmail.com>
2018-04-01 21:51:53 +0200

    Pequeña edicion Principal

--- BarBestial/src/packPrincipal/Principal.java
+++ BarBestial/src/packPrincipal/Principal.java
@@ -3,7 +3,10 @@ package packPrincipal;
 import packInterfaces.*;
 
 public class Principal {
-
+	/**
+	 * De momento no sirve
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		IPartida.getIPartida();
