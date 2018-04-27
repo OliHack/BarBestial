2196837c2048e33cb008577f3d3c446262ba5cf4
frank <frank.rb997@gmail.com>
2018-04-27 16:51:25 +0200

    pequeños retoques

--- BarBestial/src/packModelo/Loro.java
+++ BarBestial/src/packModelo/Loro.java
@@ -1,7 +1,5 @@
 package packModelo;
 
-import java.util.*;
-
 import javax.swing.JOptionPane;
 
 public class Loro implements ICompAnimalada {
@@ -12,26 +10,18 @@ public class Loro implements ICompAnimalada {
 
 	@Override
 	public void animalada() {
-		String[] choices = {"1","2","3","4"};
-	    String input = (String) JOptionPane.showInputDialog(null, "Loro: Elija el animal que quieres echar",
-	        "The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, // Use
-	                                                                        // default
-	                                                                        // icon
-	        choices, // Array of choices
-	        choices[0]); // Initial choice
-	    //System.out.println(input);
-	    int num=Integer.parseInt(input)-1; 
-		
+		String[] choices = { "1", "2", "3", "4" };
+		String input = (String) JOptionPane.showInputDialog(null, "Loro: Elija el animal que quieres echar", "LORO",
+				JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+		int num = Integer.parseInt(input) - 1;
 		CartaAnimal animal = CartasEnJuego.getCartasEnJuego().delCarta(num);
 		Calle.getCalle().addAnimal(animal);
-
 	}
 
 	@Override
 	public void animalada(int pos) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
